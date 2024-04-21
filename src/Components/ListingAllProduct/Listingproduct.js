5752fce3b960f0f272a6458bcfb50e41e7a8e2e5
narrow-space <hasanimrantri@gmail.com>
2024-04-22 00:55:38 +0600

    first commit

---
+++ src/Components/ListingAllProduct/Listingproduct.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import ListingProductMain from '../ListingPageMain/ListingProductMain'
+
+const Listingproduct = () => {
+  return (
+    <>
+    
+    <ListingProductMain/>
+    </>
+  )
+}
+
+export default Listingproduct
---
+++ src/api/Helper.js
@@ -0,0 +1,3 @@
+// export const BASE_URL="http://localhost:4001"
+export const BASE_URL="https://emart-backend-21cj.onrender.com"
+
