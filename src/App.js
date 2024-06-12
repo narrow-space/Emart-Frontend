348d16b0a5cd3d1b9014da399c1c733d1cbf428f
narrow-space <hasanimrantri@gmail.com>
2024-06-12 18:19:46 +0600

    lazy-loading added

--- src/App.js
+++ src/App.js
@@ -1,98 +1,74 @@
-import React, { useContext, useEffect, useState } from "react";
+import React, { useContext, useEffect, useState, lazy, Suspense } from "react";
 import "./App.css";
-import Layout from "./layouts/Layout.js";
-import Banner from "./Components/Banner/Banner.js";
-import Home from "./Pages/Home/Home.js";
-
-import ThemeProvider from "./Contexapi/Themecontex.js";
-import NavProvider from "./Contexapi/NavopenContex.js";
+import { Routes, Route, useLocation, useNavigate } from "react-router-dom";
 import Lottie from "lottie-react";
 import animationData from "./Animation - 1702200728330.json";
-import { Routes, Route, useLocation, useNavigate } from "react-router-dom";
-import ListingProductMain from "./Components/ListingPageMain/ListingProductMain.js";
-import ProductDetails from "./Components/ProductDetails/ProductDetails.js";
-import Notfound from "./Components/Share/Notfound.js";
-import Viewcart from "./Components/carts/Viewcart/Viewcart.js";
-import Checkout from "./Components/carts/Checkout/Checkout.js";
-import Dashboard from "./Components/user_Authentication/Dashboard.js"
-import { FaBagShopping } from "react-icons/fa6";
-import Myaccount from "./Components/user_Authentication/Myaccount.js";
-import Orderhistory from "./Components/user_Authentication/Orderhistory.js";
-import Accountdetails from "./Components/user_Authentication/Accountdetails.js";
-import Editaddress from "./Components/user_Authentication/Editaddress.js";
-import Login from "./Components/user_Authentication/Login.js";
-import Forgotpassword from "./Components/user_Authentication/Forgotpassword.js";
-import Resetpassword from "./Components/user_Authentication/Resetpassword.js";
-import Register from "./Components/user_Authentication/Register.js";
-import Shipping from "./Components/Shipping/Shipping.js";
-
-import AdminLogin from "./Components/Admin_Authentication/AdminLogin.js";
-import AdminRegister from "./Components/Admin_Authentication/AdminRegister.js";
-import AdminAccount from "./Components/Admin_Authentication/AdminAccount.js";
-import Admindashboard from "./Components/Admin_Authentication/Admindashboard.js";
-import AddBannerImages from "./Components/Admin_Authentication/AddBannerImages.js";
-
-import Addproducts from "./Components/Admin_Authentication/Addproducts.js";
-import Addcategories from "./Components/Admin_Authentication/Addcategories.js";
-import Addbrand from "./Components/Admin_Authentication/Addbrand.js";
-import Orders from "./Components/Admin_Authentication/Orders.js";
-import Settings from "./Components/Admin_Authentication/Settings.js";
-import Adminproducts from "./Components/Admin_Authentication/Adminproducts.js";
 import toast, { Toaster } from 'react-hot-toast';
-import UpdateProduct from "./Components/Admin_Authentication/UpdateProduct.js";
-import ScroolToTop from "./Components/Share/ScroolToTop.js";
-import UserProtectedRoutes from "./Components/ProtectRoutes/UserProtectedRoutes.js";
-import AdminProtectedRoutes from "./Components/ProtectRoutes/AdminProtectRoutes.js";
 import { useDispatch, useSelector } from "react-redux";
-import CartProvider, { CartopenContex } from "./Contexapi/Cartopencontex.js";
 import { jwtDecode } from 'jwt-decode';
 import { clearuserLoggedInData } from "./redux/Slice/Userauthslice/userAuthSlice.js";
 import { clearadminLoggedINData } from "./redux/Slice/adminAuthslice/adminAuthslice.js";
+import { FaBagShopping } from "react-icons/fa6";
+
+// Context Providers
+import ThemeProvider from "./Contexapi/Themecontex.js";
+import NavProvider from "./Contexapi/NavopenContex.js";
+import CartProvider, { CartopenContex } from "./Contexapi/Cartopencontex.js";
+
+// Shared Components
+import Layout from "./layouts/Layout.js";
+import ScroolToTop from "./Components/Share/ScroolToTop.js";
 import ShoppingBag from "./Components/Share/ShoppingBag.js";
 import FlotingComponentsMobile from "./Components/Share/FlotingComponentsMobile.js";
-import WishList from "./Components/Wishlist/WishList.js";
+import UserProtectedRoutes from "./Components/ProtectRoutes/UserProtectedRoutes.js";
+import AdminProtectedRoutes from "./Components/ProtectRoutes/AdminProtectRoutes.js";
+
+// Lazy Loaded Components
+const Banner = lazy(() => import("./Components/Banner/Banner.js"));
+const Home = lazy(() => import("./Pages/Home/Home.js"));
+const ListingProductMain = lazy(() => import("./Components/ListingPageMain/ListingProductMain.js"));
+const ProductDetails = lazy(() => import("./Components/ProductDetails/ProductDetails.js"));
+const Notfound = lazy(() => import("./Components/Share/Notfound.js"));
+const Viewcart = lazy(() => import("./Components/carts/Viewcart/Viewcart.js"));
+const Checkout = lazy(() => import("./Components/carts/Checkout/Checkout.js"));
+const Dashboard = lazy(() => import("./Components/user_Authentication/Dashboard.js"));
+const Myaccount = lazy(() => import("./Components/user_Authentication/Myaccount.js"));
+const Orderhistory = lazy(() => import("./Components/user_Authentication/Orderhistory.js"));
+const Accountdetails = lazy(() => import("./Components/user_Authentication/Accountdetails.js"));
+const Editaddress = lazy(() => import("./Components/user_Authentication/Editaddress.js"));
+const Login = lazy(() => import("./Components/user_Authentication/Login.js"));
+const Forgotpassword = lazy(() => import("./Components/user_Authentication/Forgotpassword.js"));
+const Resetpassword = lazy(() => import("./Components/user_Authentication/Resetpassword.js"));
+const Register = lazy(() => import("./Components/user_Authentication/Register.js"));
+const Shipping = lazy(() => import("./Components/Shipping/Shipping.js"));
+const AdminLogin = lazy(() => import("./Components/Admin_Authentication/AdminLogin.js"));
+const AdminRegister = lazy(() => import("./Components/Admin_Authentication/AdminRegister.js"));
+const AdminAccount = lazy(() => import("./Components/Admin_Authentication/AdminAccount.js"));
+const Admindashboard = lazy(() => import("./Components/Admin_Authentication/Admindashboard.js"));
+const AddBannerImages = lazy(() => import("./Components/Admin_Authentication/AddBannerImages.js"));
+const Addproducts = lazy(() => import("./Components/Admin_Authentication/Addproducts.js"));
+const Addcategories = lazy(() => import("./Components/Admin_Authentication/Addcategories.js"));
+const Addbrand = lazy(() => import("./Components/Admin_Authentication/Addbrand.js"));
+const Orders = lazy(() => import("./Components/Admin_Authentication/Orders.js"));
+const Settings = lazy(() => import("./Components/Admin_Authentication/Settings.js"));
+const Adminproducts = lazy(() => import("./Components/Admin_Authentication/Adminproducts.js"));
+const UpdateProduct = lazy(() => import("./Components/Admin_Authentication/UpdateProduct.js"));
+const WishList = lazy(() => import("./Components/Wishlist/WishList.js"));
+
 const App = () => {
   const location = useLocation();
-
   const dispatch = useDispatch();
-  const navigate = useNavigate()
-
-
-
-
+  const navigate = useNavigate();
 
   const { userLoggedInData } = useSelector((state) => state.user);
+
   useEffect(() => {
     if (location.pathname) {
       window.scrollTo({ top: 0, left: 0, behavior: "smooth" });
     }
-
   }, [location]);
 
-
-
-
-
-
-
-
-  ///Disable mouse Right Click////
-  // useEffect(() => {
-  //   // Add event listener to disable right-click
-  //   function disableRightClick(event) {
-  //     event.preventDefault();
-  //   }
-
-  //   document.body.addEventListener('contextmenu', disableRightClick);
-
-  //   // Clean up event listener on component unmount
-  //   return () => {
-  //     document.body.removeEventListener('contextmenu', disableRightClick);
-  //   };
-  // }, []);
-
-
-  ////user automatic logout when token expired//
+  // User automatic logout when token expired
   useEffect(() => {
     const token = localStorage.getItem('usertoken');
 
@@ -108,10 +84,7 @@ const App = () => {
           dispatch(clearuserLoggedInData());
           localStorage.removeItem('usertoken');
           navigate('/login');
-
-        }
-
-        else {
+        } else {
           // Calculate time until token expiration
           const timeUntilExpiration = (expirationTime - currentTime) * 1000;
 
@@ -120,7 +93,6 @@ const App = () => {
             dispatch(clearuserLoggedInData());
             localStorage.removeItem('usertoken');
             navigate('/login');
-
           }, timeUntilExpiration);
 
           // Clean up the timer when the component unmounts or when the token changes
@@ -130,9 +102,7 @@ const App = () => {
     }
   }, [navigate, userLoggedInData]);
 
-
-
-  ////admin autometic logout when token expired//
+  // Admin automatic logout when token expired
   useEffect(() => {
     const token = localStorage.getItem('admintoken');
 
@@ -148,10 +118,7 @@ const App = () => {
           dispatch(clearadminLoggedINData());
           localStorage.removeItem('admintoken');
           navigate('/');
-
-        }
-
-        else {
+        } else {
           // Calculate time until token expiration
           const timeUntilExpiration = (expirationTime - currentTime) * 1000;
 
@@ -160,7 +127,6 @@ const App = () => {
             dispatch(clearadminLoggedINData());
             localStorage.removeItem('admintoken');
             navigate('/');
-
           }, timeUntilExpiration);
 
           // Clean up the timer when the component unmounts or when the token changes
@@ -170,143 +136,71 @@ const App = () => {
     }
   }, [navigate, userLoggedInData]);
 
-
-
-
-
   const token = localStorage.getItem('admintoken');
-
-
-
-
-
-
-
 
   return (
     <div className="webkit">
-
       <ThemeProvider>
-
         <NavProvider>
-
-          {
-            token ? null : <>
+          {token ? null : (
+            <>
               <ShoppingBag />
               <FlotingComponentsMobile />
             </>
-          }
+          )}
 
+                <Suspense fallback={<div className="flex h-[100vh] items-center justify-center">
 
-          <Routes>
+                  <Lottie animationData={animationData}  style={{ width: 200, height: 200 }} />
+                </div>}>
 
-            {/* Admin Router */}
+            <Routes>
+              {/* Admin Router */}
+              <Route exact={true} path="/admin/login" element={<Layout><AdminLogin /></Layout>} />
+              <Route exact={true} path="/admin/register" element={<Layout><AdminRegister /></Layout>} />
 
-            <Route exact={true} path="/admin/login" element={<Layout><AdminLogin /></Layout>} />
-            <Route exact={true} path="/admin/register" element={<Layout><AdminRegister /></Layout>} />
-
-            {/*Admin account Nested Route  */}
-            <Route path="adminaccount" element={<AdminProtectedRoutes Components={AdminAccount} />}>
-              <Route
-                exact={true}
-                path="dashboard"
-                element={<Admindashboard />}
-              />
-              <Route
-                exact={true}
-                path="bannerimages"
-                element={<AddBannerImages />}
-              />
-              <Route
-                exact={true}
-                path="addproducts"
-                element={<Addproducts />}
-              />
-              <Route
-                exact={true}
-                path="updateproduct/:id"
-                element={<UpdateProduct />}
-              />
-              <Route
-                exact={true}
-                path="addcategories"
-                element={<Addcategories />}
-              />
-              <Route
-                exact={true}
-                path="addbrand"
-                element={<Addbrand />}
-              />
-              <Route
-                exact={true}
-                path="orders"
-                element={<Orders />}
-              />
-              <Route
-                exact={true}
-                path="settings"
-                element={<Settings />}
-              />
-              <Route
-                exact={true}
-                path="products"
-                element={<Adminproducts />}
-              />
-            </Route>
-
-            {/* User Router */}
-            <Route exact={true} path="/" element={<Layout><Home /></Layout>} />
-
-            <Route
-              exact={true}
-              path="/products-filter"
-              element={<Layout><ListingProductMain /></Layout>}
-            />
-
-            <Route
-              exact={true}
-              path="/allproduct/:id"
-              element={<Layout><ProductDetails /></Layout>}
-            />
-            <Route
-              exact={true}
-              path="/viewcart"
-              element={<Layout><Viewcart /></Layout>}
-            />
-            <Route
-              exact={true}
-              path="/viewcart/checkout"
-              element={<Layout><Checkout /></Layout>}
-            />
-            <Route
-              exact={true}
-              path="/wishlist"
-              element={<Layout><WishList /></Layout>}
-            />
-            {/*user account Nested Route  */}
-            <>
-              <Route path="myaccount" element={<UserProtectedRoutes Components={Myaccount} />}>
-                <Route index path="dashboard" element={<Dashboard />}></Route>
-                <Route index path="orderhistory" element={<Orderhistory />}></Route>
-                <Route path="accountdetails" element={<Accountdetails />}></Route>
-                <Route path="editaddress" element={<Editaddress />}></Route>
+              {/* Admin account Nested Route */}
+              <Route path="adminaccount" element={<AdminProtectedRoutes Components={AdminAccount} />}>
+                <Route exact={true} path="dashboard" element={<Admindashboard />} />
+                <Route exact={true} path="bannerimages" element={<AddBannerImages />} />
+                <Route exact={true} path="addproducts" element={<Addproducts />} />
+                <Route exact={true} path="updateproduct/:id" element={<UpdateProduct />} />
+                <Route exact={true} path="addcategories" element={<Addcategories />} />
+                <Route exact={true} path="addbrand" element={<Addbrand />} />
+                <Route exact={true} path="orders" element={<Orders />} />
+                <Route exact={true} path="settings" element={<Settings />} />
+                <Route exact={true} path="products" element={<Adminproducts />} />
               </Route>
-            </>
 
-            <Route path="/login" element={<Layout><Login /></Layout>} />
-            <Route path="/register" element={<Layout><Register /></Layout>} />
-            <Route path="/forgotpassword" element={<Layout><Forgotpassword /></Layout>} />
-            <Route path="/resetpassword/:id/:token" element={<Layout><Resetpassword /></Layout>} />
-            <Route path="/shipping" element={<Layout><Shipping /></Layout>} />
+              {/* User Router */}
+              <Route exact={true} path="/" element={<Layout><Home /></Layout>} />
+              <Route exact={true} path="/products-filter" element={<Layout><ListingProductMain /></Layout>} />
+              <Route exact={true} path="/allproduct/:id" element={<Layout><ProductDetails /></Layout>} />
+              <Route exact={true} path="/viewcart" element={<Layout><Viewcart /></Layout>} />
+              <Route exact={true} path="/viewcart/checkout" element={<Layout><Checkout /></Layout>} />
+              <Route exact={true} path="/wishlist" element={<Layout><WishList /></Layout>} />
 
-            <Route exact={true} path="*" element={<Layout><Notfound /></Layout>} />
-          </Routes>
+              {/* User account Nested Route */}
+              <Route path="myaccount" element={<UserProtectedRoutes Components={Myaccount} />}>
+                <Route index path="dashboard" element={<Dashboard />} />
+                <Route index path="orderhistory" element={<Orderhistory />} />
+                <Route path="accountdetails" element={<Accountdetails />} />
+                <Route path="editaddress" element={<Editaddress />} />
+              </Route>
+
+              <Route path="/login" element={<Layout><Login /></Layout>} />
+              <Route path="/register" element={<Layout><Register /></Layout>} />
+              <Route path="/forgotpassword" element={<Layout><Forgotpassword /></Layout>} />
+              <Route path="/resetpassword/:id/:token" element={<Layout><Resetpassword /></Layout>} />
+              <Route path="/shipping" element={<Layout><Shipping /></Layout>} />
+              <Route exact={true} path="*" element={<Layout><Notfound /></Layout>} />
+            </Routes>
+          </Suspense>
           <Toaster />
         </NavProvider>
-
       </ThemeProvider>
       <ScroolToTop />
-    </div >
+    </div>
   );
 };
 
